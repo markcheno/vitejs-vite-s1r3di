0eba766928d96bf024823b077bf16ceb4d06c8d1
markcheno <mark@cheno.net>
2023-04-05 20:37:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-s1r3di
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-s1r3di)
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import Pocketbase from 'pocketbase';
+
+let pb = null;
+
+onMounted(() => {
+  pb = new Pocketbase('https://cheno.xyz');
+});
+
+</script>
+
+<template>
+  <h1>Hello, world!</h1>
+</template>
+
+<style scoped></style>
